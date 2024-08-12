e2740f9db60f78a5a6206e92f1c92d52a7c8c776
bruno-pedron <brunorupaner09@gmail.com>
2024-08-12 10:30:33 -0400

    restaurant

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes -$12', 'Eggs Benedict -$22.99', 'Oatmeal -$22.99', 'Frittata -$22.99'];
+const mainCourseMenu = ['Steak -$24.99', 'Pasta -$17.99', 'Burger -$16.99', 'Salmon -$26.99'];
+const dessertMenu = ['Cake -$7.99', 'Ice Cream -$2.99', 'Pudding -$2.99', 'Fruit Salad -$3.99'];
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
         document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
  let mainCourseItem = '';
