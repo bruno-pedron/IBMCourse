4598d9bcddadd1656f9f8e6045d83b22dc8c1cb6
bruno-pedron <brunorupaner09@gmail.com>
2024-08-13 13:22:37 -0400

    debugging

---
+++ DebugCode/debug_code.js
@@ -0,0 +1,45 @@
+    
+    function performOperation() {
+    // Get user input from input fields
+    let num1 = parseInt(document.getElementById('input1').value);
+    let num2 = parseInt(document.getElementById('input2').value);
+    // Check if inputs are valid numbers
+    if (!isNaN(num1) && !isNaN(num2)) {
+    // Perform the operation
+                    let result = [(multiply(num1, num2)), (add(num1, num2)) , (div(num1, num2))]; 
+                    // Display the result
+                    displayResult(result);
+                } else {
+                    displayResult('Please enter valid numbers');
+                }
+            }
+
+            function multiply(a, b) {
+                // Introduce a debugger statement to pause execution
+                debugger;
+
+                // Multiply the numbers
+                return a * b;
+            }
+
+            function add(a, b) {
+                // Introduce a debugger statement to pause execution
+                debugger;
+
+                // add the numbers
+                return a + b;
+            }
+
+            function div(a, b) {
+                // Introduce a debugger statement to pause execution
+                debugger;
+
+                // Divide the numbers
+                return a/b;
+            }
+
+            function displayResult(result) {
+                // Display the result in the paragraph element
+                const resultElement = document.getElementById('result');
+                resultElement.textContent = `The result for Adding, Multiplying and Dividing are : ${result}`;
+            }
